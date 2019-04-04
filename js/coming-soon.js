475cc65e927f2d3ffcf5a071c5015f4282fef9d5
Elliot J. Voris <elliot@voris.me>
2019-04-04 12:11:22 -0500

    Customized The Web App
    Basically it's a magic-8-ball-type thing

--- js/coming-soon.js
+++ js/coming-soon.js
@@ -1,6 +1,45 @@
 (function($) {
   "use strict"; // Start of use strict
 
-  // No JS
+  // Elliot's JS
+  var debateObject = {};
+  debateObject.possibleAnswers = [
+    "without a doubt",
+    "definitely not",
+    "yes",
+    "no",
+    "she probably found that on the internet",
+    "have her make her case again",
+    "don't count on it",
+    "it is certain",
+    "most likely",
+    "count on it",
+    "goteem",
+    "outlook not so good",
+    "my sources point to yes, but they also said hillary would win",
+    "better not tell you that",
+    "don't be stupid",
+    "your mom",
+    "yeah right"
+  ];
+  debateObject.getAnswer = function(){
+    var randomNumber = Math.random();
+    var randomAnswer = Math.floor(randomNumber * this.possibleAnswers.length);
+    var answer = this.possibleAnswers[randomAnswer];
+
+    $('#answerText').text( answer );
+    $('#debateAnswer').fadeIn(3000);
+
+    console.log( answer );
+  }
+
+  $('#debateAnswer').hide();
+  var onClick = function(){
+    $('#debateTopic').val("");
+    $('#debateAnswer').hide();
+    debateObject.getAnswer();
+  }
+
+  $("#debateButton").click( onClick );
 
 })(jQuery); // End of use strict
